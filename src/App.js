28be75392a34e3378df02e69b112193eb74b01ed
torianne02 <victoria.fluharty@gmail.com>
2019-04-05 15:14:03 -0700

    imports navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import NavBar from './components/NavBar';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
       <div className="app">
+        <NavBar />
         <Switch>
         </Switch>
       </div>
