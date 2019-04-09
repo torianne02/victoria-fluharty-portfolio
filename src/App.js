7bf92f4377fb81112d47089dc39c0b793321bfbb
torianne02 <victoria.fluharty@gmail.com>
2019-04-09 15:36:55 -0700

    adds about route path adds to-dos via comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import About from './components/About';
 import './App.css';
 
 const App = () => {
@@ -9,6 +10,9 @@ const App = () => {
       <div className="app">
         <NavBar />
         <Switch>
+          <Route exact path="/about" component={About} />
+          // need to add contact container and route
+          // need to add project container and route
         </Switch>
       </div>
     </Router>
