5fa8c96c107afab7defc5521cbadac8508a8ccec
torianne02 <victoria.fluharty@gmail.com>
2019-04-09 15:25:31 -0700

    creates projects folder creates project component adds to-dos via commented out code

---
+++ src/components/projects/Project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// will need to import Card, cardText, CardBody, CardSubtitle, and CardTitle
+// from reactstrap
+
+const Project = () => {
+  return (
+    // will put card for each individual project here
+  )
+}
+
+export default Project
+
+
+// still deciding whether I will hard code the projects in or create a DB
