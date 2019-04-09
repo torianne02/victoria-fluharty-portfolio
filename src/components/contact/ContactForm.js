9c4acaef017df947609005d973ccd9c3d2754b60
torianne02 <victoria.fluharty@gmail.com>
2019-04-09 15:30:12 -0700

    creates contact folder creates ContactForm component and to-do notes basic structure complete only

---
+++ src/components/contact/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// need to import form attr from reactstrap
+
+const ContactForm = () => {
+  return (
+    // need to create form for Contact
+    // need to learn how to attach gmail
+  )
+}
+
+export default ContactForm
