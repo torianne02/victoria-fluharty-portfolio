0bedadd2fd56abc17359e035513f464086ca6a55
torianne02 <victoria.fluharty@gmail.com>
2019-04-09 15:33:26 -0700

    creates thank you component creates basic structure and to-dos

---
+++ src/components/contact/ThankYou.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// need to import card attr from reactstrap
+
+// THIS WILL BE A 'THANK YOU FOR SENDING ME AN EMAIL - I WILL REPLY SHORTLY'
+
+class ThankYou extends Component {
+  render() {
+    return (
+      // will add thank you card info
+      // will put it in a div tag
+    )
+  }
+}
+
+export default ThankYou
