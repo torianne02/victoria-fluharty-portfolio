10923d83c55ad8aacc71eaa7a634791617257e38
torianne02 <victoria.fluharty@gmail.com>
2019-04-12 10:58:09 -0700

    creates new Projects.js folder imports card attr from reactstrap basic template of component

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+
+const Projects = () => {
+  return (
+
+  )
+}
+
+export default Projects
