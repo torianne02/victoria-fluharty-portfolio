a5730a6c126893e525a8cb56afb9a37ab161f052
torianne02 <victoria.fluharty@gmail.com>
2019-04-12 10:34:20 -0700

    comments out Project.js will be used in future use with creation of DB currently working on MVP

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,14 +1,16 @@
-import React from 'react';
-// will need to import Card, cardText, CardBody, CardSubtitle, and CardTitle
-// from reactstrap
+// FUTURE USE -- WORKING ON MVP 
 
-const Project = () => {
-  return (
-    // will put card for each individual project here
-  )
-}
-
-export default Project
-
-
-// still deciding whether I will hard code the projects in or create a DB
+// import React from 'react';
+// // will need to import Card, cardText, CardBody, CardSubtitle, and CardTitle
+// // from reactstrap
+//
+// const Project = () => {
+//   return (
+//     // will put card for each individual project here
+//   )
+// }
+//
+// export default Project
+//
+//
+// // still deciding whether I will hard code the projects in or create a DB
