286b2eb41906e8944264326cf6c2bf8cae7ad1e8
torianne02 <victoria.fluharty@gmail.com>
2019-04-15 16:47:34 -0700

    deletes prev github logo adds official ones adds sm and md github logos

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Row, Col, Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 const Projects = () => {
   return (
-
+    <div className="projects">
+      <Row>
+        <Col>
+          <Card className="project">
+            <CardTitle>Crawford Wedding</CardTitle>
+            <CardSubtitle>A multi-functional wedding web-application.</CardSubtitle>
+            <CardText>Stack:</CardText>
+            // insert either languages by text or images
+            // insert github link via github button as logo
+          </Card>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
