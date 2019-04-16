9135c1d014935e70d7378889e45a279c83781f8d
torianne02 <victoria.fluharty@gmail.com>
2019-04-15 17:28:22 -0700

    implements CardBody attr

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -8,33 +8,39 @@ const Projects = () => {
         <Col>
           <Card className="project">
             // add CardImg for image of project
-            <CardTitle>Personal Portfolio</CardTitle>
-            <CardSubtitle>Portfolio web-applicaiton built to display projects
-              and other vital information.</CardSubtitle>
-            <CardText>Stack:</CardText>
-            // insert either languages by text or images - React.js, Reactstrap, Bootstrap, Javascript, CSS
-            // insert github link via github button as sm-logo - https://github.com/torianne02/victoria-fluharty-portfolio
+            <CardBody>
+              <CardTitle>Personal Portfolio</CardTitle>
+              <CardSubtitle>Portfolio web-applicaiton built to display projects
+                and other vital information.</CardSubtitle>
+              <CardText>Stack:</CardText>
+              // insert either languages by text or images - React.js, Reactstrap, Bootstrap, Javascript, CSS
+              // insert github link via github button as sm-logo - https://github.com/torianne02/victoria-fluharty-portfolio
+            </CardBody>
           </Card>
         </Col>
         <Col>
           <Card className="project">
             // add CardImg for image of project
-            <CardTitle>Crawford Wedding</CardTitle>
-            <CardSubtitle>A multi-functional wedding web-application.</CardSubtitle>
-            <CardText>Stack:</CardText>
-            // insert either languages by text or images - React.js, Ruby on Rails, JavaScript, SQLite, HTML, CSS, Reactstrap, Bootstrap
-            // insert github link via github button as sm-logo - https://github.com/torianne02/crawford-wedding-frontend
+            <CardBody>
+              <CardTitle>Crawford Wedding</CardTitle>
+              <CardSubtitle>A multi-functional wedding web-application.</CardSubtitle>
+              <CardText>Stack:</CardText>
+              // insert either languages by text or images - React.js, Ruby on Rails, JavaScript, SQLite, HTML, CSS, Reactstrap, Bootstrap
+              // insert github link via github button as sm-logo - https://github.com/torianne02/crawford-wedding-frontend
+            </CardBody>
           </Card>
         </Col>
         <Col>
           <Card className="project">
             // add CardImg for image of project
-            <CardTitle>PintTrackr 2.0</CardTitle>
-            <CardSubtitle>A web-application used to track beers that one has
-              tried over the years.</CardSubtitle>
-            <CardText>Stack:</CardText>
-            // insert either languages by text or images - Ruby on Rails, JavaScript, SQLite, HTML, CSS, AJAX, jQuery, JSON
-            // insert github link via github button as sm-logo - https://github.com/torianne02/pint-trackr-rails-js
+            <CardBody>
+              <CardTitle>PintTrackr 2.0</CardTitle>
+              <CardSubtitle>A web-application used to track beers that one has
+                tried over the years.</CardSubtitle>
+              <CardText>Stack:</CardText>
+              // insert either languages by text or images - Ruby on Rails, JavaScript, SQLite, HTML, CSS, AJAX, jQuery, JSON
+              // insert github link via github button as sm-logo - https://github.com/torianne02/pint-trackr-rails-js
+            </CardBody>
           </Card>
         </Col>
       </Row>
@@ -42,34 +48,40 @@ const Projects = () => {
         <Col>
           <Card className="project">
             // add CardImg for image of project
-            <CardTitle>Weather Caster</CardTitle>
-            <CardSubtitle>A simple current forecast web-application.</CardSubtitle>
-            <CardText>Stack:</CardText>
-            // insert either languages by text or images - React.js, Reactstrap, Bootstrap, Javascript, CSS
-            // insert github link via github button as sm-logo - https://github.com/torianne02/weather-caster
+            <CardBody>
+              <CardTitle>Weather Caster</CardTitle>
+              <CardSubtitle>A simple current forecast web-application.</CardSubtitle>
+              <CardText>Stack:</CardText>
+              // insert either languages by text or images - React.js, Reactstrap, Bootstrap, Javascript, CSS
+              // insert github link via github button as sm-logo - https://github.com/torianne02/weather-caster
+            </CardBody>
           </Card>
         </Col>
         <Col>
           <Card className="project">
             // add CardImg for image of project
-            <CardTitle>PintTrackr 1.0</CardTitle>
-            <CardSubtitle>A web-application used to track beers that one has
-              tried over the years.</CardSubtitle>
-            <CardText>Stack:</CardText>
-            // insert either languages by text or images - Ruby on Rails, ActiveRecord
-            // insert github link via github button as sm-logo - https://github.com/torianne02/pint-trackr-rails
+            <CardBody>
+              <CardTitle>PintTrackr 1.0</CardTitle>
+              <CardSubtitle>A web-application used to track beers that one has
+                tried over the years.</CardSubtitle>
+              <CardText>Stack:</CardText>
+              // insert either languages by text or images - Ruby on Rails, ActiveRecord
+              // insert github link via github button as sm-logo - https://github.com/torianne02/pint-trackr-rails
+            </CardBody>
           </Card>
         </Col>
         <Col>
           <Card className="project">
             // add CardImg for image of project
-            <CardTitle>DayBooks</CardTitle>
-            <CardSubtitle>A Ruby CLI gem that allows a user to comb through a
-              list of all books written and published by famous author, Sylvia
-              Day.</CardSubtitle>
-            <CardText>Stack:</CardText>
-            // insert either languages by text or images - Ruby
-            // insert github link via github button as sm-logo - https://github.com/torianne02/CLI-Project-Day-Library
+            <CardBody>
+              <CardTitle>DayBooks</CardTitle>
+              <CardSubtitle>A Ruby CLI gem that allows a user to comb through a
+                list of all books written and published by famous author, Sylvia
+                Day.</CardSubtitle>
+              <CardText>Stack:</CardText>
+              // insert either languages by text or images - Ruby
+              // insert github link via github button as sm-logo - https://github.com/torianne02/CLI-Project-Day-Library
+            </CardBody>
           </Card>
         </Col>
       </Row>
