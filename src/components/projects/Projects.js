c1aa1f0b8f4f4909690d3280d106c29b114163a6
torianne02 <victoria.fluharty@gmail.com>
2019-04-15 17:00:13 -0700

    creates project cards using reactstrap in Projects.js imports CardImg creates a card for 4 projects uses rows and cols for clean display adds todos/commented code

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,11 +7,46 @@ const Projects = () => {
       <Row>
         <Col>
           <Card className="project">
+            // add CardImg for image of project
             <CardTitle>Crawford Wedding</CardTitle>
             <CardSubtitle>A multi-functional wedding web-application.</CardSubtitle>
             <CardText>Stack:</CardText>
-            // insert either languages by text or images
-            // insert github link via github button as logo
+            // insert either languages by text or images - React.js, Ruby on Rails, JavaScript, SQLite, HTML, CSS, Reactstrap, Bootstrap
+            // insert github link via github button as sm-logo - https://github.com/torianne02/crawford-wedding-frontend
+          </Card>
+        </Col>
+        <Col>
+          <Card className="project">
+            // add CardImg for image of project
+            <CardTitle>PintTrackr 2.0</CardTitle>
+            <CardSubtitle>A web-application used to track beers that one has
+              tried over the years.</CardSubtitle>
+            <CardText>Stack:</CardText>
+            // insert either languages by text or images - Ruby on Rails, JavaScript, SQLite, HTML, CSS, AJAX, jQuery, JSON
+            // insert github link via github button as sm-logo - https://github.com/torianne02/pint-trackr-rails-js
+          </Card>
+        </Col>
+        <Col>
+          <Card className="project">
+            // add CardImg for image of project
+            <CardTitle>Weather Caster</CardTitle>
+            <CardSubtitle>A simple current forecast web-application.</CardSubtitle>
+            <CardText>Stack:</CardText>
+            // insert either languages by text or images - React.js, Reactstrap, Bootstrap, Javascript, CSS
+            // insert github link via github button as sm-logo - https://github.com/torianne02/weather-caster
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card className="project">
+            // add CardImg for image of project
+            <CardTitle>PintTrackr 1.0</CardTitle>
+            <CardSubtitle>A web-application used to track beers that one has 
+              tried over the years.</CardSubtitle>
+            <CardText>Stack:</CardText>
+            // insert either languages by text or images - Ruby on Rails, ActiveRecord
+            // insert github link via github button as sm-logo - https://github.com/torianne02/pint-trackr-rails
           </Card>
         </Col>
       </Row>
