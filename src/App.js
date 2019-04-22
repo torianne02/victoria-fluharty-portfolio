bfabee150102bad8de84f4586138dcb61cd20d3f
torianne02 <victoria.fluharty@gmail.com>
2019-04-22 16:51:33 -0700

    imports Projects component to App.js creates route for Projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import About from './components/About';
+import Projects from './components/Projects';
 import './App.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <NavBar />
         <Switch>
           <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
           // need to add contact container and route
           // need to add project container and route
         </Switch>
