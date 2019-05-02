966034f1a507e9ae9378041f5a19843f59cb184a
torianne02 <victoria.fluharty@gmail.com>
2019-05-02 15:32:20 -0700

    adds temporary text to about me page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about-me">
-
+      <h1>Hello 👋🏼 I am Victoria.</h1>
+      <h5>A creative, passionate, and driven software engineer with experience in Ruby and Javascript frameworks that strives to write clean, efficient code.</h5>
     </div>
   )
 }
