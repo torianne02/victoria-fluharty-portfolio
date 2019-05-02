e1829e6eeb46597df942c55d342a486de1372007
torianne02 <victoria.fluharty@gmail.com>
2019-05-02 16:15:32 -0700

    updates About.js imports Media from reactstrap imports main img file adds div tags

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Media } from 'reactstrap';
+import mainImg from '../images/rr-library-vf.jpg';
 
 const About = () => {
   return (
     <div className="about-me">
-      <h1>Hello 👋🏼 I am Victoria.</h1>
-      <h5>A creative, passionate, and driven software engineer with experience in Ruby and Javascript frameworks that strives to write clean, efficient code.</h5>
+      <div className="main-img">
+        <Media top right className="main-img">
+          <Media object src={mainImg} alt="Photo of me, Victoria." />
+        </Media>
+      </div>
+      <div className="about-info">
+        <h1>Hello 👋🏼 I am Victoria.</h1>
+        <h5>A creative, passionate, and driven software engineer with experience in Ruby and Javascript frameworks that strives to write clean, efficient code.</h5>
+      </div>
     </div>
   )
 }
