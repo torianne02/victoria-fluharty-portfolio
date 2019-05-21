c49de400e23a230e1db45c5f5330c6799d29d9d6
torianne02 <victoria.fluharty@gmail.com>
2019-05-21 15:40:43 -0700

    creates contact container

---
+++ src/containers/ContactContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ContactForm from '../components/contact/ContactForm';
+
+class ContactContainer extends Component {
+
+}
+
+export default ContactContainer
