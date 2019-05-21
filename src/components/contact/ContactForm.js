6c03ee7040283eec8567053dc76dd7fc8a609862
torianne02 <victoria.fluharty@gmail.com>
2019-05-21 15:38:18 -0700

    creates basic layout of form adds notes of todos

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import { Container, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
+// need to add onChange, onSubmit, and value attr to each form group
+
 const ContactForm = () => {
   return (
-    // need to create form for Contact
-    // need to learn how to attach gmail
+    <Container className="contact-form">
+      <Form>
+        <Col>
+          <FormGroup>
+            <Label for="name" hidden>Name</Label>
+            <Input type="text"
+              name="name"
+              placeholder="Full Name" />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="email" hidden>Email</Label>
+            <Input type="email"
+              name="email"
+              placeholder="Email" />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="message" hidden>Message</Label>
+            <Input type="text"
+              name="message"
+              placeholder="Message" />
+          </FormGroup>
+        </Col>
+        <Button>Submit</Button>
+      </Form>
+    </Container>
   )
 }
 
