f646f5f80651ae615495a7334ee66b2b319bcd9f
torianne02 <victoria.fluharty@gmail.com>
2019-05-21 15:52:17 -0700

    adds logic to ContactContainer creates constructor method creates handleOnChange and handleOnSubmit methods creates validation method creates a render method to pull up thank you component when submitted

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 import ContactForm from '../components/contact/ContactForm';
+import { Redirect } from 'react-router-dom';
 
 class ContactContainer extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      submitted: false,
+    }
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+
+    if (this.validate()) {
+      renderThankYou()
+    }
+  }
+
+  validate = () => {
+    if (!this.state.name || !this.state.email) {
+      return alert('Oops! Please make sure to provide your name and email.')
+    } else if (!this.state.message) {
+      return alert('Please make sure to include a message so that I can respond to your inquiry to the best of my abilities.')
+    } else {
+      return true
+    }
+  }
+
+  renderThankYou() {
+    return (
+      <Redirect to='/thank-you' />
+    )
+  }
 }
 
 export default ContactContainer
