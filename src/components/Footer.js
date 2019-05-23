a8c6739530613504d4323bb3b83f0486ce4be7c7
torianne02 <victoria.fluharty@gmail.com>
2019-05-23 16:44:28 -0700

    comments out code for footer component will not use for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,24 @@
-import React from 'react';
-
-const Footer = () => {
-  return (
-  )
-}
-
-export default Footer
+// import React from 'react';
+// import { Row } from 'reactstrap';
+// import githubLogo from '../images/logos/github-logo-sm.png';
+// import twitterLogo from '../images/logos/twitter-logo-sm.png';
+//
+// const Footer = () => {
+//   return (
+//     <footer className="footer">
+//       <Row className="social-links">
+//         <a href="https://github.com/torianne02">
+//           <img className="social-icon" src={githubLogo} alt="Github logo as profile link" />
+//         </a>
+//         <a href="https://dev.to/torianne02">
+//           <img className="social-icon" src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Victoria Fluharty's DEV Profile" height="32px" width="32px" />
+//         </a>
+//         <a href="https://twitter.com/_torrborr">
+//           <img className="social-icon" src={twitterLogo} alt="Github logo as profile link" />
+//         </a>
+//       </Row>
+//     </footer>
+//   )
+// }
+//
+// export default Footer
