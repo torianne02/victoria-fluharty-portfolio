6e4cd901e5a684d67bf0c0a7cd7d7acded6c607f
torianne02 <victoria.fluharty@gmail.com>
2019-05-24 13:20:55 -0700

    creates home container

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+import mainImg from '../images/rr-library-vf.jpg';
+import githubLogo from '../images/logos/github-logo-sm.png';
+import twitterLogo from '../images/logos/twitter-logo-sm.png';
+import linkedinLogo from '../images/logos/linkedin-logo-sm.png';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Row className="intro">
+        <Col className="intro-info">
+          <h1>Hello 👋🏼 I am Victoria.</h1>
+
+          <Row className="social-links">
+            <a href="https://github.com/torianne02">
+              <img className="social-icon" src={githubLogo} alt="Github logo as profile link" />
+            </a>
+            <a href="https://dev.to/torianne02">
+              <img className="social-icon" src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Victoria Fluharty's DEV Profile" height="32px" width="32px" />
+            </a>
+            <a href="https://twitter.com/_torrborr">
+              <img className="social-icon" src={twitterLogo} alt="Twitter logo as profile link" />
+            </a>
+            <a href="https://www.linkedin.com/in/victoria-fluharty-741129b4/">
+              <img className="social-icon" src={linkedinLogo} alt="LinkedIn logo as profile link" />
+            </a>
+          </Row>
+
+          <h5>A creative, passionate, and driven software engineer with experience
+            in Ruby and Javascript frameworks that strives to write clean,
+            efficient code.</h5>
+        </Col>
+        <Col className="main-img">
+            <img className="main-img" src={mainImg} alt="Photo of me, Victoria." />
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Home
