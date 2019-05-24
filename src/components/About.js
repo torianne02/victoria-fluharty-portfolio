de5c8b07d4bb4799d8ca1c6e99d5e7cc0ffe760c
torianne02 <victoria.fluharty@gmail.com>
2019-05-24 13:21:29 -0700

    moves intro and main image to home component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
-import mainImg from '../images/rr-library-vf.jpg';
 import javascriptLogo from '../images/languages/javascript-logo.png';
 import railsLogo from '../images/languages/rails-logo.png';
 import rubyLogo from '../images/languages/ruby-logo.png';
@@ -8,40 +7,10 @@ import reactLogo from '../images/languages/react-logo.png';
 import reduxLogo from '../images/languages/redux-logo.png';
 import sqliteLogo from '../images/languages/sqlite-logo.png';
 import postgresLogo from '../images/languages/postgres-logo.png';
-import githubLogo from '../images/logos/github-logo-sm.png';
-import twitterLogo from '../images/logos/twitter-logo-sm.png';
-import linkedinLogo from '../images/logos/linkedin-logo-sm.png';
 
 const About = () => {
   return (
     <div className="about-me">
-      <Row className="intro">
-        <Col className="about-info">
-          <h1>Hello 👋🏼 I am Victoria.</h1>
-
-          <Row className="social-links">
-            <a href="https://github.com/torianne02">
-              <img className="social-icon" src={githubLogo} alt="Github logo as profile link" />
-            </a>
-            <a href="https://dev.to/torianne02">
-              <img className="social-icon" src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Victoria Fluharty's DEV Profile" height="32px" width="32px" />
-            </a>
-            <a href="https://twitter.com/_torrborr">
-              <img className="social-icon" src={twitterLogo} alt="Twitter logo as profile link" />
-            </a>
-            <a href="https://www.linkedin.com/in/victoria-fluharty-741129b4/">
-              <img className="social-icon" src={linkedinLogo} alt="LinkedIn logo as profile link" />
-            </a>
-          </Row>
-
-          <h5>A creative, passionate, and driven software engineer with experience
-            in Ruby and Javascript frameworks that strives to write clean,
-            efficient code.</h5>
-        </Col>
-        <Col className="main-img">
-            <img className="main-img" src={mainImg} alt="Photo of me, Victoria." />
-        </Col>
-      </Row>
       <Row className="stack">
         <Col sm="3" className="stack-title">
           <h1>My Stack</h1>
@@ -56,6 +25,23 @@ const About = () => {
             <img className="lang-logo" src={sqliteLogo} alt="SQLite Logo" />
             <img className="lang-logo" src={postgresLogo} alt="PostgreSQL Logo" />
           </Row>
+        </Col>
+      </Row>
+      <Row className="interests">
+        <Col sm="3" className="interests-title">
+          <h1>My Interests</h1>
+        </Col>
+        <Col className="interests-info">
+          <ul>
+            <li><h5>I love animals - I am a fur momma to two rambunctious cats.</h5></li>
+            <li><h5>I absolutely love being outdoors, especially if it involves hiking
+               and camping.</h5></li>
+            <li><h5>I enjoy traveling internationally, but I have a soft spot for
+               traveling my own country. My ultimate goal is visiting all 50 states -
+               currently at 36.</h5></li>
+            <li><h5>My passion truly lies within helping others, no matter how big or
+            small of an impact I make. </h5></li>
+          </ul>
         </Col>
       </Row>
     </div>
