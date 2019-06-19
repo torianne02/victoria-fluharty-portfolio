8fee748d20055b45cfc62b8d60f2b14115d54d56
torianne02 <victoria.fluharty@gmail.com>
2019-06-19 11:35:46 -0700

    updates code for easier read

--- src/components/About.js
+++ src/components/About.js
@@ -17,11 +17,12 @@ const About = () => {
         </Col>
         <Col className="background-info">
           <ul>
-            <li><h5>I obtained my M.S. degree in athletic training in 2016.</h5></li>
-            <li><h5>I previously worked as a <b>head athletic
-              trainer</b> for a <b>professional ice hockey</b> team in the <b>ECHL</b>.</h5></li>
-            <li><h5>As of February 2019, I am an alumn of Flatiron School's online <b>software
-              engineering bootcamp</b>.</h5></li>
+            <li><h5>I obtained my M.S. degree in athletic training from California
+              University of Pennsylvania.</h5></li>
+            <li><h5>I previously worked as a <b>head athletic trainer</b> for a
+              <b> professional ice hockey</b> team in the <b>ECHL.</b></h5></li>
+            <li><h5>As of February 2019, I am an alumn of Flatiron School's online
+              <b> software engineering bootcamp.</b></h5></li>
           </ul>
         </Col>
       </Row>
@@ -55,7 +56,7 @@ const About = () => {
                currently at 36.</h5></li>
             <li><h5>My passion truly lies within helping others, no matter how big or
               small of an impact I make. </h5></li>
-            <li><h5>Ice hockey is my favorite sport to watch - Let's go Pens!</h5></li>
+            <li><h5>Ice hockey is my favorite sport to watch - <b>Let's go Pens!</b></h5></li>
           </ul>
         </Col>
       </Row>
