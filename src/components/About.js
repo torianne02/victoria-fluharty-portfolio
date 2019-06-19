0eebdfe0527c180cf458b2c17c2084e8fa9c3561
torianne02 <victoria.fluharty@gmail.com>
2019-06-19 11:25:17 -0700

    adds section to about.js creates background section and adds text updates content in interests section reorders rows

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,20 @@ import postgresLogo from '../images/languages/postgres-logo.png';
 const About = () => {
   return (
     <div className="about-me">
+      <Row className="background">
+        <Col sm="3" className="background-title">
+          <h1>Background</h1>
+        </Col>
+        <Col className="background-info">
+          <ul>
+            <li><h5>I obtained my M.S. degree in athletic training in 2016.</h5></li>
+            <li><h5>I previously worked as a <b>head athletic
+              trainer</b> for a <b>professional ice hockey</b> team in the <b>ECHL</b>.</h5></li>
+            <li><h5>As of February 2019, I am an alumn of Flatiron School's online <b>software
+              engineering bootcamp</b>.</h5></li>
+          </ul>
+        </Col>
+      </Row>
       <Row className="stack">
         <Col sm="3" className="stack-title">
           <h1>Tech Stack</h1>
@@ -29,20 +43,19 @@ const About = () => {
       </Row>
       <Row className="interests">
         <Col sm="3" className="interests-title">
-          <h1>My Interests</h1>
+          <h1>Interests</h1>
         </Col>
         <Col className="interests-info">
           <ul>
             <li><h5>I love animals - fur momma to two rambunctious cats.</h5></li>
-            <li><h5>I absolutely love being outdoors, especially if it involves hiking
+            <li><h5>Being outdoors, especially if it involves hiking
                and camping.</h5></li>
             <li><h5>I enjoy traveling internationally, but I have a soft spot for
-               traveling my own country. My ultimate goal is visiting all 50 states -
+               traveling my own country. My ultimate goal is visiting all 50 states by 30 -
                currently at 36.</h5></li>
             <li><h5>My passion truly lies within helping others, no matter how big or
               small of an impact I make. </h5></li>
-            <li><h5>I love ice hockey and previously worked as a <b>head athletic
-              trainer</b> for a <b>professional ice hockey</b> team in the <b>ECHL</b>.</h5></li>
+            <li><h5>Ice hockey is my favorite sport to watch - Let's go Pens!</h5></li>
           </ul>
         </Col>
       </Row>
