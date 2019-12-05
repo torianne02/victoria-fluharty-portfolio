0619d2e8c8202db03659e1e9c071b2d60928fb39
torianne02 <victoria.fluharty@gmail.com>
2019-12-05 15:43:35 -0800

    adds new bullet point in about me background section

--- src/components/About.js
+++ src/components/About.js
@@ -23,6 +23,7 @@ const About = () => {
               <b> professional ice hockey</b> team in the <b>ECHL.</b></h5></li>
             <li><h5>As of February 2019, I am an alumn of Flatiron School's online
               <b> software engineering bootcamp.</b></h5></li>
+            <li><h5>In August 2019, I helped Flatiron School open up the San Francisco campus as a <b>software engineer coach</b>.</h5></li>
           </ul>
         </Col>
       </Row>
